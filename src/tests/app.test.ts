b43c5a6bea5ff1012d96e1c001a5b54e82bf6c1b
Louis Ledoux <19322697+louisledoux@users.noreply.github.com>
2023-11-28 13:52:33 +0100

    feat(App): init command-line functionalities with tests

---
+++ src/tests/app.test.ts
@@ -0,0 +1,45 @@
+import {describe, expect, it} from "vitest";
+import {app, processArguments} from "../app";
+
+describe("app", () => {
+  it('should be defined', () => {
+    expect(() => app()).toBeDefined();
+  });
+})
+
+describe("processArguments", () => {
+  it('should return an error when no arguments is given in command-line', () => {
+    const args: string[] = [];
+    expect(() => processArguments(args)).toThrowError();
+  });
+
+  it('should return an error when more than one argument is given at the same time', () => {
+    const args = ["--filter=ry", "--count"];
+    expect (() => processArguments(args)).toThrowError();
+  });
+
+  it('should return an error when an incorrect argument is given in command-line', () => {
+    const args = ["--xyz"];
+    expect(() => processArguments(args)).toThrowError();
+  });
+
+  it('should return the response of the filter service when the filter argument is called in command-line', () => {
+    const args = ["--filter=ry"];
+    expect(processArguments(args)).toBe('Filter service');
+  });
+
+  it('should return an error when the filter argument is called without parameters in command-line', () => {
+    const args = ["--filter"];
+    expect(() => processArguments(args)).toThrowError();
+  });
+
+  it('should return an error when the filter argument is called with an empty parameter in command-line', () => {
+    const args = ["--filter="];
+    expect(() => processArguments(args)).toThrowError();
+  });
+
+  it('should return the response of the count service when the count argument is called in command-line', () => {
+    const args = ["--count"];
+    expect(processArguments(args)).toBe('Count service');
+  });
+})

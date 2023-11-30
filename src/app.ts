e24d1f58af1def6a855c427a3bb24b227154f82b
Louis Ledoux <19322697+louisledoux@users.noreply.github.com>
2023-11-30 14:43:09 +0100

    refactor(App): improve result logs

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import {processArguments} from "./utils/commands";
+import util from 'util';
 
 // Extract command line arguments skipping the first two (node app.js)
 const commandLineArgs = process.argv.slice(2);
 const result = processArguments(commandLineArgs);
-console.log(result)
+console.log(util.inspect(result,{showHidden: false, depth: null, colors: true}))
